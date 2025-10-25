4b9e9da1bd39010f1b8758795065c12a5b9200af
Dewansh Gupta <82531138+dewansh999@users.noreply.github.com>
2025-10-26 00:15:49 +0530

    Update DemoRestController.java

--- src/main/java/in/expedia/rest/DemoRestController.java
+++ src/main/java/in/expedia/rest/DemoRestController.java
@@ -11,11 +11,7 @@ public class DemoRestController {
 		return "<h3>Welcome Admin :)</h3>";
 	}
 
-	@GetMapping(value = "/user")
-	public String user() {
-		return "<h3>Hello User :)</h3>";
-	}
-
+	
 	@GetMapping(value = "/")
 	public String welcome() {
 		return "<h3>Welcome :)</h3>";
